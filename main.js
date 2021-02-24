9d655ff6e51ec2790c79a2b1b42655138e5e92d2
Kamila Stolarska <kamila0212@gmail.com>
2021-02-24 23:21:15 +0000

    dropdown in calculator

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ bars.addEventListener('click', function () {
     navList.classList.toggle('active');
 });
 
+//Calculator
+const selectedOption = document.querySelector('.selected-option');
+const dropdown = document.querySelector('.custom-select .dropdown');
+
+selectedOption.addEventListener('click', function(){
+    dropdown.classList.toggle("open");
+})
+
+const dropdownOptions = document.querySelectorAll('.dropdown div');
+
+for(let i=0; i<dropdownOptions.length; i++){
+    dropdownOptions[i].addEventListener('click',function(){
+        selectedOption.textContent = dropdownOptions[i].textContent;
+        selectedOption.style.color = '#000000';
+        selectedOption.dataset.value = dropdownOptions[i].dataset.value;
+        
+        dropdown.classList.toggle("open");
+    })
+}
